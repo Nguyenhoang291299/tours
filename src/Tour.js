135872d92e71b131823c1592546d86273a454dc9
Nguyenhoang291299 <hoang11761311@gmail.com>
2022-11-07 15:27:37 +0700

    main

---
+++ src/Tour.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const Tour = (id, image, info, price, name) => {
+
+    return <article className="single-tour">
+        <img src={image} alt={name} />
+        <footer>
+            <div className="tour-info">
+                <h4>{name}</h4>
+                <h4 className="tour-price">{price}</h4>
+                <p>{info}</p>
+                <button className="del-btn">not interested</button>
+            </div>
+        </footer>
+    </article>
+};
+
+export default Tour
